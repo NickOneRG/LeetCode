983ba9fa6feff79f6d501625a39fb4260d81157c
NickOneRG <robidron0199@gmail.com>
2024-01-28 09:14:08 +0500

    weekly chalange +

--- LeetCode/100179.Minimize-OR-of-Remaining-Elements-Using-Operations.py
+++ LeetCode/100179.Minimize-OR-of-Remaining-Elements-Using-Operations.py
@@ -8,32 +8,23 @@ t = TimeSet()
 
 class Solution:
     def minOrAfterOperations(self, nums: List[int], k: int) -> int:
-        # Convert all numbers in nums to negative
-        # Because Python's heapq is a min heap, we need to convert it to a max heap
         nums = [-num for num in nums]
-
-        # Turn nums into a heap
         heapq.heapify(nums)
 
-        # Perform operations
         while k > 0 and len(nums) > 1:
-            # Pop two smallest (largest in original) numbers
             a = -heapq.heappop(nums)
             b = -heapq.heappop(nums)
-
-            # Push their AND back into the heap
-            heapq.heappush(nums, -(a & b))
-
             k -= 1
 
-        # Return the OR of the remaining numbers
+            heapq.heappush(nums, -(a & b))
+
         return -heapq.heappop(nums)
 
 
 sol = Solution()
-print(sol.minOrAfterOperations([3, 5, 3, 2, 7], 2))  # Output: 3
-print(sol.minOrAfterOperations([7, 3, 15, 14, 2, 8], 4))  # Output: 2
-print(sol.minOrAfterOperations([10, 7, 10, 3, 9, 14, 9, 4], 1))  # Output: 15
+print(sol.minOrAfterOperations([3, 5, 3, 2, 7], 2)) 
+print(sol.minOrAfterOperations([7, 3, 15, 14, 2, 8], 4)) 
+print(sol.minOrAfterOperations([10, 7, 10, 3, 9, 14, 9, 4], 1)) 
 
 
 class Solution:
@@ -65,6 +56,7 @@ print(m.minOrAfterOperations([10, 7, 10, 3, 9, 14, 9, 4], 1))
 # print(call)
 print(t.hisobla(time, 1))
 sol = Solution()
-print(sol.minOrAfterOperations([3, 5, 3, 2, 7], 2))  # Output: 3
-print(sol.minOrAfterOperations([7, 3, 15, 14, 2, 8], 4))  # Output: 2
-print(sol.minOrAfterOperations([10, 7, 10, 3, 9, 14, 9, 4], 1))  # Output: 15
+
+print(sol.minOrAfterOperations([3, 5, 3, 2, 7], 2))  
+print(sol.minOrAfterOperations([7, 3, 15, 14, 2, 8], 4))  
+print(sol.minOrAfterOperations([10, 7, 10, 3, 9, 14, 9, 4], 1))  
