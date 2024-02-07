5ba1c79bdaf2c082391c4a5b38df31c03af4c623
NickOneRG <robidron0199@gmail.com>
2024-02-07 10:24:17 +0500

    Daily chalange 7.02.2024

--- LeetCode/451.sort-characters-by-frequency.py
+++ LeetCode/451.sort-characters-by-frequency.py
@@ -9,7 +9,9 @@ t = TimeSet()
 #
 
 # @lc code=start
-class Solution:
+
+
+class Solution:  # First Approaches  ==> good running time
     def frequencySort(self, s: str) -> str:
         group = dict()
 
@@ -27,11 +29,37 @@ class Solution:
         return res
 
         
+class Solution:  # Second Approaches  ==> using sort in place, defaultdict and range 
+    def frequencySort(self, s: str) -> str:
+        memo, group = Counter(s), defaultdict(list)
+
+        for key in memo.keys():
+            group[memo[key]].append(key)
+
+        memo = list(group.keys())
+        memo.sort()
+
+        res = ""
+        for i in range(len(memo)-1, -1, -1):
+            for val in sorted(group[memo[i]]):
+                
+                res += val * memo[i]
+
+        return res
+    
+
+class Solution:  # Third Approaches  ==> one liner, + testing "most_common" built in methods
+    def frequencySort(self, s: str) -> str:
+
+        return "".join(key * val for key, val in Counter(s).most_common())
+
+
+
 # @lc code=end
 
 
 m = Solution()
 time = t.hisobla()
-print(m.frequencySort("treeAabb"))
+print(m.frequencySort("cccaaa"))
 # print(call)
 print(t.hisobla(time, 1))
